d36945ca80f6835497248595e92c55afbd3765d4
Muhammad Aamir <muhammad.aamir@remotebase.com>
2021-12-26 16:35:02 +0500

    changes in lambda function reponse

--- amplify/backend/function/amplifydemoapp55ca3df9/src/index.js
+++ amplify/backend/function/amplifydemoapp55ca3df9/src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     console.log(event);
     // TODO implement
     const customerId = event.pathParameters.customerId;
-    const customer = {'customerId':customerId, 'customerName':'Customer_'+customerId};
+    const customer = {'customer Id':customerId, 'customer Name':'Customer '+customerId};
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
