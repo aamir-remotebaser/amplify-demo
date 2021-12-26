b6376e54a00d8c49ceb0f6f484d100806b89be95
Muhammad Aamir <muhammad.aamir@remotebase.com>
2021-12-26 19:55:50 +0500

    Response JSON updated

--- amplify/backend/function/amplifydemoapp55ca3df9/src/index.js
+++ amplify/backend/function/amplifydemoapp55ca3df9/src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     console.log(event);
     // TODO implement
     const customerId = event.pathParameters.customerId;
-    const customer = {'customer_Id':customerId, 'customer_Name':'Customer '+customerId};
+    const customer = {'customer_Id':customerId, 'customer_Name':'Customer '+customerId, 'updated':'Yes'};
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
